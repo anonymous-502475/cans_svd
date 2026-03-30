59bf1ea0dd5b2af98b55f2efa96dde2cf33b0709
anonymous-502475 <anonymous-502475@proton.me>
2026-03-30 13:43:29 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+from src.casn_svd import cans_svd
+from src.svd_pseudo import svd_pseudo
+from src.svd_tikhonov import svd_tikhonov
+
+
+__all__ = [
+    "cans_svd",
+    "svd_pseudo",
+    "svd_tikhonov",
+]
